3251867c69be2f8915a7c68cc579cb12b675db96
deniskatsapov <63019948+deniskatsapov@users.noreply.github.com>
2020-05-27 23:38:21 +0300

    0.0.1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(".burger-menu").click(function () {
+   $(".burger-menu").toggleClass("burger-menu__active") 
+});
+
+// categories
+$(".categories-nav").click(function () {
+    $(".categories-nav__list").toggleClass("nav-list__active")
+})
